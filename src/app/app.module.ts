c33c4da5ef0e6cc4fd9af1d5d7538ec2aaad66c8
lazaros <psyzed66@gmail.com>
2022-10-13 15:23:28 +0300

    added angular material to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatePostsComponent } from './posts/create-posts/create-posts.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CreatePostsComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/posts/create-posts/create-posts.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-posts',
+  templateUrl: './create-posts.component.html',
+  styleUrls: ['./create-posts.component.css'],
+})
+export class CreatePostsComponent implements OnInit {
+  enteredValue: string = '';
+  newPost: string = 'No Content!';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
+}
