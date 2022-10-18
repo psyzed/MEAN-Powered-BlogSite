8cd55f7d7efc6488bf8206dc8f9405e83a1e20a8
lazaros <psyzed66@gmail.com>
2022-10-18 11:02:53 +0300

    created the mongoose post schema

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const Post = require("../backend/models/post");
+
 const app = express();
 
 app.use(express.json());
@@ -20,7 +22,11 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  // console.log(req.body);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
   console.log(post);
   res.status(201).json({
     message: "Post Added Successfully",
