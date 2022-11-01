6c6a7ea14cb555095dc6b624cc1249b8ba6d374d
lazaros <psyzed66@gmail.com>
2022-11-01 13:45:54 +0200

    refactored multer config to a seperate file

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,46 +2,17 @@ const express = require("express");
 
 const PostsController = require("../controllers/posts");
 
-const multer = require("multer");
-
-const Post = require("../models/post");
 const checkAuthMiddleware = require("../middleware/check-auth");
+const multerConfig = require("../middleware/multer-config");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg",
-};
-
-const storageConfig = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid Mime Type!");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(" ").join("-");
-    const extension = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + extension);
-  },
-});
-
-router.post(
-  "",
-  checkAuthMiddleware,
-  multer({ storage: storageConfig }).single("image"),
-  PostsController.createPost
-);
+router.post("", checkAuthMiddleware, multerConfig, PostsController.createPost);
 
 router.put(
   "/:id",
   checkAuthMiddleware,
-  multer({ storage: storageConfig }).single("image"),
+  multerConfig,
   PostsController.updatePost
 );
 
